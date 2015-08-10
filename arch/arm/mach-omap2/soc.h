edac7c17272332cd9441cedaef5c5e57726911c1
Keerthy <j-keerthy@ti.com>
2015-08-10 14:58:23 +0530

    ARM: AM43xx: Introduce a separate soc_is function for am438x series of SoCs
    EPOS evms are fitted with a separate family of am43xx SoCs and are named
    am438x series. Adding a separate soc_is function to identify that
    particular series of SoCs. This can be done to avoid unnecessarily
    registering hwmods like rtc when not needed on EPOS evms.
    
    Signed-off-by: Keerthy <j-keerthy@ti.com>

--- arch/arm/mach-omap2/soc.h
+++ arch/arm/mach-omap2/soc.h
@@ -238,6 +238,7 @@ IS_AM_SUBCLASS(437x, 0x437)
 #define soc_is_am335x()			0
 #define soc_is_am43xx()			0
 #define soc_is_am437x()			0
+#define soc_is_am438x()			0
 #define cpu_is_omap44xx()		0
 #define cpu_is_omap443x()		0
 #define cpu_is_omap446x()		0
@@ -371,8 +372,10 @@ IS_OMAP_TYPE(3430, 0x3430)
 #ifdef	CONFIG_SOC_AM43XX
 # undef soc_is_am43xx
 # undef soc_is_am437x
-# define soc_is_am43xx()		is_am43xx()
-# define soc_is_am437x()		is_am437x()
+# undef soc_is_am438x
+# define soc_is_am43xx()		of_machine_is_compatible("ti,am43")
+# define soc_is_am437x()		of_machine_is_compatible("ti,am4372")
+# define soc_is_am438x()		of_machine_is_compatible("ti,am438x")
 #endif
 
 # if defined(CONFIG_ARCH_OMAP4)
